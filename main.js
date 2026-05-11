6606f22ddca55f019aa810dcd510ba01c0981a55
Myosotis-904 <siyuchen928@gmail.com>
2026-05-11 14:48:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,8 @@ class GameScene extends Phaser.Scene {
 
         // 舞台位置（這裡可以改座標）
         this.stage = this.add.image(600, 300, 'stage')
-       .setOrigin(0.5, 1);
+       .setOrigin(0.5, 1),
+       .setScale(scale);
 
         let scaleX = this.scale.width / this.map.width;
         let scaleY = this.scale.height / this.map.height;
